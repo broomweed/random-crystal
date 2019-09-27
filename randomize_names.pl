3288d20c787139b1b2ba3fc06f98169b9443d16a
broomweed <milesabaker@gmail.com>
2019-09-27 01:10:12 -0700

    update name randomizer to produce more pleasant results

--- randomize_names.pl
+++ randomize_names.pl
@@ -1,13 +1,24 @@
 #!/usr/bin/perl
 
 use v5.010;
-use List::Util qw/sum shuffle/;
+use List::Util qw/sum shuffle min uniq/;
 use Data::Dumper;
 use File::Copy;
 use MIME::Base64;
 
 use constant NAMES_START => 0x053384;
 use constant MAX_NAME_LEN => 10;
+
+my @weird_arg = grep { /^--weird=/ } @ARGV;
+my $weirdness = 0.5;
+
+if (@weird_arg) {
+    $weird_arg[0] =~ /=([-0-9.]+)$/;
+    $weirdness = $1;
+    die "Weirdness must be a number > 0\n" if $weirdness <= 0;
+}
+
+@ARGV = grep { $_ !~ /^--weird=/ } @ARGV;
 
 my @words;
 
@@ -17,7 +28,7 @@ my @words;
     @words = <$wordfile>;
     chomp @words;
 
-    say "Read @{[scalar @words]} words";
+    say "Read @{[scalar @words]} standard pokemon names";
 }
 
 my %stats;
@@ -47,9 +58,9 @@ for my $word (@words) {
         my $prevpiece = $i > 0 ? $wordparts[$i-1] : '';
         my $nextpiece = $i < $#wordparts ? $wordparts[$i+1] : '';
 
-        my $type = ($piece =~ /[aeiouy%]/ ? "v" : "c") x length $piece;
-        my $prevtype = ($prevpiece =~ /[aeiouy%]/ ? "v" : "c") x length $prevpiece;
-        my $nexttype = ($nextpiece =~ /[aeiouy%]/ ? "v" : "c") x length $nextpiece;
+        my $type = ($piece =~ /[aeiouy%]/ ? "v" : "c") x min(length $piece, 3);
+        my $prevtype = ($prevpiece =~ /[aeiouy%]/ ? "v" : "c") x min(length $prevpiece, 3);
+        my $nexttype = ($nextpiece =~ /[aeiouy%]/ ? "v" : "c") x min(length $nextpiece, 3);
 
         my $position;
         if ($i == 0) {
@@ -61,6 +72,12 @@ for my $word (@words) {
         }
         $stats{$position}{$piece} ++;
     }
+}
+
+# Raise stats of positions to a power for slightly more normal-looking names
+# (or less normal, as the case may be...)
+for my $type (keys %stats) {
+    @{$stats{$type}}{keys %{$stats{$type}}} = map { $_ ** (1/$weirdness) } values %{$stats{$type}};
 }
 
 my @newwords = ();
@@ -81,9 +98,9 @@ for my $word (@words) {
         my $prevpiece = $i > 0 ? $wordparts[$i-1] : '';
         my $nextpiece = $i < $#wordparts ? $wordparts[$i+1] : '';
 
-        my $type = ($piece =~ /[aeiouy%]/ ? "v" : "c") x length $piece;
-        my $prevtype = ($prevpiece =~ /[aeiouy%]/ ? "v" : "c") x length $prevpiece;
-        my $nexttype = ($nextpiece =~ /[aeiouy%]/ ? "v" : "c") x length $nextpiece;
+        my $type = ($piece =~ /[aeiouy%]/ ? "v" : "c") x min(length $piece, 3);
+        my $prevtype = ($prevpiece =~ /[aeiouy%]/ ? "v" : "c") x min(length $prevpiece, 3);
+        my $nexttype = ($nextpiece =~ /[aeiouy%]/ ? "v" : "c") x min(length $nextpiece, 3);
 
         my $position;
         if ($i == 0) {
@@ -139,8 +156,11 @@ for my $i ('0', '1'..'9') {
     $byte ++;
 }
 
-my $inrom = $ARGV[0] or die "please specify a rom file to scramble; usage: $0 <infile> <outfile>";
-my $outrom = $ARGV[1] or die "please specify an output file; usage: $0 <infile> <outfile>";
+my $usage = "usage: $0 <infile> <outfile> [--weird=<number>]\n" .
+            "       weird: amplify uncommon name parts. default is 0.5";
+
+my $inrom = $ARGV[0] or die "please specify a rom file to scramble\n$usage";
+my $outrom = $ARGV[1] or die "please specify an output file\n$usage";
 
 copy($inrom, $outrom) or die "Couldn't create output ROM file $outrom: $!.\n";
 
@@ -152,29 +172,42 @@ sub capitalize_after {
     return map { join $char, @$_ } map { [ map { ucfirst $_ } @$_ ] } map { [ split $char, $_ ] } @words;
 }
 
-@newwords = grep { length $_ <= MAX_NAME_LEN } @newwords;
-@newwords = capitalize_after ' ', @newwords;
-@newwords = capitalize_after '-', @newwords;
-
 # some bad words aren't very funny, like racial/etc slurs
 # but i don't want people seeing such words on my github page
 # so i just base64'd them. you can decode them if you're curious which words i banned
-my $badwords = decode_base64('ZmFnfG5pZ3xjdW50fHRhcmQ=');
+my $badwords = decode_base64('ZmFnfG5pZ3xjdW50fHRhcmR8cmFwZQ==');
 @newwords = grep { $_ !~ /$badwords/i } @newwords;
 
 @newwords = grep { $_ !~ /tr$/ } @newwords; # 'feraligatr' is dumb due to text limits
 @newwords = grep { $_ !~ /ncc/ } @newwords; # 'minccino' etc don't work well here
 @newwords = grep { $_ !~ /rrl/ } @newwords; # likewise 'purrloin'
+@newwords = grep { $_ !~ /ktr/ } @newwords; # 'eelektrik' is weird spelled on purpose
 @newwords = grep { $_ !~ /:/ } @newwords; # 'type: null' I think is weird (up to you I guess)
+@newwords = grep { $_ !~ /[ui]w/ } @newwords; # bad clusters
+@newwords = grep { $_ !~ /[^aeiouy][^aeiouyl]e$/ } @newwords; # silent e looks bad after consonant clusters
+@newwords = grep { $_ !~ /e[^aeiouy]e$/ } @newwords; # unclear how to pronounce these
+@newwords = grep { $_ !~ /rir|lir|rur|lul/ } @newwords; # weird sequence
+
+@newwords = grep { length $_ <= MAX_NAME_LEN } @newwords;
+@newwords = capitalize_after ' ', @newwords;
+@newwords = capitalize_after '-', @newwords;
+
+@newwords = uniq @newwords;
 
 @newwords = shuffle @newwords;
+
+if (@newwords < 251) {
+    die "Sorry, we weren't able to generate enough unique names.\nPerhaps the weirdness setting was too low?";
+}
 
 for my $index (0..255) {
     my @bytes = map { $CHARTABLE{$_} } split '', $newwords[$index];
 
     push @bytes, 0x50 while @bytes < MAX_NAME_LEN;
 
-    say ((sprintf "%".MAX_NAME_LEN."s", $newwords[$index]), " => ", (join ':', map { sprintf "%02X", $_ } @bytes));
+    my $formatted_name = sprintf "%".MAX_NAME_LEN."s", $newwords[$index];
+    $formatted_name =~ s/%/é/g;
+    say $formatted_name, " => ", (join ':', map { sprintf "%02X", $_ } @bytes);
 
     seek $OUT, NAMES_START + $index * MAX_NAME_LEN, 0;
     print { $OUT } pack "C*", @bytes;
